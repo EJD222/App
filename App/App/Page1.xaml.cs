94f758459d2ca352753a24b84b394611525c3eb5
Elmalia Jane Diaz <eljdiaz500@gmail.com>
2024-02-16 15:08:05 +0800

    Add project files.

---
+++ App/App/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private async void Clicked_page2 (object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+        private async void Clicked_page3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page3());
+        }
+        private void MyCodeClicked(object sender, EventArgs e)
+        {
+            myEntry.Text = "Welcome to my World";
+            myEntry.TextColor = Color.Green;
+        }   
+    }
+}
